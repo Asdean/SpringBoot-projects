e7f7380802b0c105a8112ea61861e256e840a05d
Asdean <3349981242@qq.com>
2021-09-19 16:10:00 +0800

    commit

--- springboot-mybatis/src/main/java/com/example/springbootmybatis/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/example/springbootmybatis/SpringbootMybatisApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 // 声明mapper包为mapper接口, 无需在mapper类上加注解@Mapper
 // @MapperScan(basePackages = "com.example.springbootmybatis.mapper")
+// @EnableTransactionManagement    //开启事务(可选项)
 public class SpringbootMybatisApplication {
 
     public static void main(String[] args) {
